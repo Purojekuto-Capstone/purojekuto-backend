73052dc9174fb0f58458126f7f48e4e9b7d1a402
Santiago Salgado <jssalgadog@gmail.com>
2021-06-21 16:21:57 -0500

    feat(projects/models.py): Build the models Activity and ActivityCategory

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -10,7 +10,7 @@ class ProjectCategory(models.Model):
     modified_date = models.DateField('Modified date', auto_now = True, auto_now_add = False)
     deleted_date = models.DateField('Delete', auto_now = True, auto_now_add = False)
 
-    category_name = models.CharField('Descripcion', max_length = 50, unique = True, null = False, blank = False)
+    project_category_name = models.CharField('Descripcion', max_length = 50, unique = True, null = False, blank = False)
 
     @property
     def _history_user(self):
@@ -26,7 +26,7 @@ class ProjectCategory(models.Model):
         verbose_name_plural = 'Projects Categories'
 
     def __str__(self):
-        return self.category_name
+        return self.project_category_name
 class Project(models.Model):
 
     # Define fields here
@@ -37,7 +37,7 @@ class Project(models.Model):
     deleted_date = models.DateField('Delete', auto_now = True, auto_now_add = False)
 
     project_name = models.CharField(max_length=30)
-    category_project = models.ForeignKey(ProjectCategory, on_delete = models.CASCADE, verbose_name = "Project Category")
+    project_category = models.ForeignKey(ProjectCategory, on_delete = models.CASCADE, verbose_name = "Project Category")
     historical = HistoricalRecords()
 
     @property
@@ -54,3 +54,56 @@ class Project(models.Model):
     def __str__(self):
         """Unicode representation of Product."""
         return self.project_name
+class ActivityCategory(models.Model):
+
+        # Define fields here
+    id = models.AutoField(primary_key = True)
+    state = models.BooleanField('State', default = True)
+    created_date = models.DateField('Creation date', auto_now = False, auto_now_add = True)
+    modified_date = models.DateField('Modified date', auto_now = True, auto_now_add = False)
+    deleted_date = models.DateField('Delete', auto_now = True, auto_now_add = False)
+
+    activity_category_name = models.CharField('Descripcion', max_length = 50, unique = True, null = False, blank = False)
+
+    @property
+    def _history_user(self):
+        return self.changed_by
+
+    @_history_user.setter
+    def _history_user(self, value):
+        self.changed_by = value
+
+    class Meta:
+
+        verbose_name = 'Activity Category'
+        verbose_name_plural = 'Activities Categories'
+
+    def __str__(self):
+        return self.activity_category_name
+class Activity(models.Model):
+
+    # Define fields here
+    id = models.AutoField(primary_key = True)
+    state = models.BooleanField('State', default = True)
+    created_date = models.DateField('Creation date', auto_now = False, auto_now_add = True)
+    modified_date = models.DateField('Modified date', auto_now = True, auto_now_add = False)
+    deleted_date = models.DateField('Delete', auto_now = True, auto_now_add = False)
+
+    activity_name = models.CharField(max_length=30)
+    activity_category = models.ForeignKey(ActivityCategory, on_delete = models.CASCADE, verbose_name = "Project Category")
+    historical = HistoricalRecords()
+
+    @property
+    def _history_date(self):
+        return self.__history_date
+
+    @_history_date.setter
+    def _history_date(self, value):
+        self.__history_date = value
+    class Meta:
+        verbose_name = 'Activity'
+        verbose_name_plural = 'Activities'
+
+    def __str__(self):
+        """Unicode representation of Product."""
+        return self.activity_name
