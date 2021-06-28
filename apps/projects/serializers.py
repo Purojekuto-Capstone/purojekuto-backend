b76825876a33d9dba29c66d1c7ba673b7b3be455
Enrique Perez <enrike_084@hotmail.com>
2021-06-28 16:11:54 -0500

    refactor(projects): Erasing serializers.py because it is not longer needed

--- apps/projects/serializers.py
+++
@@ -1,8 +0,0 @@
-from projects.models import User
-from rest_framework import serializers
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["name", "token"]
