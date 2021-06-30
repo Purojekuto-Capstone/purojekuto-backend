63c6ab85c53446dd0a04c974245c68dd2835b986
Enrique Perez <enrike_084@hotmail.com>
2021-06-30 04:09:17 -0500

    fix(deploy): Trying to fix static files on deploy attemp4

--- purojekutoBackend/settings.py
+++ purojekutoBackend/settings.py
@@ -174,4 +174,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = "/staticfiles/"
