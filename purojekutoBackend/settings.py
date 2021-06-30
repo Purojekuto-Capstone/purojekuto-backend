aacc5d4a6c383f43632dcbcd76ac7428c2c6bbf5
Enrique Perez <enrike_084@hotmail.com>
2021-06-30 03:32:16 -0500

    fix(docs): Fixing static files for swagger ui

--- purojekutoBackend/settings.py
+++ purojekutoBackend/settings.py
@@ -76,6 +76,7 @@ SWAGGER_SETTINGS = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -169,3 +170,8 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
